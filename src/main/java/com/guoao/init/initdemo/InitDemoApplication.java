c5d1d04df25b6adf86c861f35bcef644a65bdba9
guoao <guoao.ga@alibaba-inc.com>
2020-01-14 11:41:59 +0800

    init

---
+++ src/main/java/com/guoao/init/initdemo/InitDemoApplication.java
@@ -0,0 +1,13 @@
+package com.guoao.init.initdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InitDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InitDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guoao/init/initdemo/InitDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guoao.init.initdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InitDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
